fd9a4efca7321b54936cafcd30b571f2edac3b19
Ben Lengerich <blengerich@gmail.com>
2022-04-06 07:58:03 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(name='scContextualized',
+      packages=['scContextualized'],
+      version='0.0.0',
+      install_requires=[
+          'pytorch-lightning',
+          'torch',
+          'numpy',
+          'tqdm',
+          'scikit-learn',
+          'python-igraph',
+          'matplotlib',
+          'pandas',
+          'umap-learn',
+          'interpret',
+          'tensorflow>=2.4.0',
+          'tensorflow-addons',
+          'numpy>=1.19.2',
+          'ipywidgets',
+          'torchvision',
+      ],
+)
