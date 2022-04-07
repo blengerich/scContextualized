15ecada0353be0c085592a775f41cbd1c6278a69
Ben Lengerich <blengerich@gmail.com>
2022-04-07 14:53:16 -0400

    allow bootstrap in plotting

--- scContextualized/plot_utils.py
+++ scContextualized/plot_utils.py
@@ -29,7 +29,7 @@ def simple_plot(xs, ys, x_label, y_label,
     plt.show()
 
 
-def plot_homogeneous_context(ncr, trainer, make_dataloader, C, X, Y, encoders, C_means, C_stds,
+def plot_homogeneous_context(predict_params, C, X, Y, encoders, C_means, C_stds,
     ylabel="Odds Ratio of Outcome", C_vis=None, n_vis=1000, min_effect_size=1.1):
     print("Estimating Homogeneous Contextual Effects.")
     if C_vis is None:
@@ -42,13 +42,12 @@ def plot_homogeneous_context(ncr, trainer, make_dataloader, C, X, Y, encoders, C
         C_j = C_vis.copy()
         C_j[:, :j] = 0.
         C_j[:, j+1:] = 0.
-        all_dataloader = make_dataloader(
+        (_, mus) = predict_params(
             utils.prepend_zero(C_j),
             utils.prepend_zero(np.zeros((len(C_j), X.shape[1]))),
-            utils.prepend_zero(np.zeros((len(C_j), Y.shape[1]))),
-            batch_size=16)
-        (models, mus) = trainer.predict_params(ncr, all_dataloader)
-        models = np.squeeze(models[1:]) # Heterogeneous Effects
+            utils.prepend_zero(np.zeros((len(C_j), Y.shape[1])))
+        )
+        #models = np.squeeze(models[1:]) # Heterogeneous Effects
         mus = np.squeeze(mus[1:]) # Homogeneous Effects
         x_classes = encoders[j].classes_
         x_ticks = (np.array(list(range(len(x_classes)))) - C_means[j]) / C_stds[j]
@@ -60,16 +59,15 @@ def plot_homogeneous_context(ncr, trainer, make_dataloader, C, X, Y, encoders, C
                 x_ticks=x_ticks, x_ticklabels=x_classes)
 
 
-def plot_homogeneous_tx(ncr, trainer, make_dataloader, C, X, Y, X_names,
+def plot_homogeneous_tx(predict_params, C, X, Y, X_names,
     ylabel="Odds Ratio of Outcome", min_effect_size=1.1):
     C_vis = np.zeros_like(C.values)
     X_vis = make_grid_mat(X, 1000)
-    all_dataloader = make_dataloader(
+    (models, mus) = predict_params(
         utils.prepend_zero(C_vis),
         utils.prepend_zero(np.zeros((len(C_vis), X.shape[1]))),
-                                    utils.prepend_zero(np.zeros((len(C_vis), Y.shape[1]))),
-                                    batch_size=16)
-    (models, mus) = trainer.predict_params(ncr, all_dataloader)
+        utils.prepend_zero(np.zeros((len(C_vis), Y.shape[1]))),
+    )
     models = np.squeeze(models[1:]) # Heterogeneous Effects
     homogeneous_tx_effects = np.mean(models, axis=0)
     effects = []
@@ -87,7 +85,7 @@ def plot_homogeneous_tx(ncr, trainer, make_dataloader, C, X, Y, X_names,
                 y_label=ylabel)
 
 
-def plot_heterogeneous(ncr, trainer, make_dataloader, C, X, Y, encoders, C_means, C_stds,
+def plot_heterogeneous(predict_params, C, X, Y, encoders, C_means, C_stds,
     X_names, ylabel="Influence of ", min_effect_size=0.003, n_vis=1000):
 
     C_vis = make_C_vis(C, n_vis)
@@ -95,11 +93,11 @@ def plot_heterogeneous(ncr, trainer, make_dataloader, C, X, Y, encoders, C_means
         C_j = C_vis.copy()
         C_j[:, :j] = 0.
         C_j[:, j+1:] = 0.
-        all_dataloader = make_dataloader(utils.prepend_zero(C_j),
-                                        utils.prepend_zero(np.zeros((len(C_j), X.shape[1]))),
-                                        utils.prepend_zero(np.zeros((len(C_j), Y.shape[1]))),
-                                        batch_size=16)
-        (models, mus) = trainer.predict_params(ncr, all_dataloader)
+        (models, mus) = predict_params(
+            utils.prepend_zero(C_j),
+            utils.prepend_zero(np.zeros((len(C_j), X.shape[1]))),
+            utils.prepend_zero(np.zeros((len(C_j), Y.shape[1])))
+        )
         models = np.squeeze(models[1:]) # Heterogeneous Effects
         heterogeneous_effects = models - np.mean(models, axis=0)
 
@@ -114,8 +112,9 @@ def plot_heterogeneous(ncr, trainer, make_dataloader, C, X, Y, encoders, C_means
                     x_ticks=x_ticks, x_ticklabels=x_classes)
 
 
-def plot_hallucinations(ncr, trainer, make_dataloader, X, C, Y, models, mus, compressor,
-    target_probs=np.linspace(0.1, 0.9, 9)):
+def plot_hallucinations(predict_y, X, C, Y, models, mus, compressor,
+    target_probs=np.linspace(0.1, 0.9, 9)
+    ):
     plt.figure(figsize=(10, 10))
     hallucinated_all = []
 
@@ -129,13 +128,11 @@ def plot_hallucinations(ncr, trainer, make_dataloader, X, C, Y, models, mus, com
         deltas = -diffs / sensitivities
         deltas = np.expand_dims(deltas, 1)
         hallucinated = X + deltas*models
-        # TODO: pass in predict function
-        hallucinated_dataloader = make_dataloader(
+        hallucinated_preds = predict_y(
             utils.prepend_zero(C),
-                                                 utils.prepend_zero(hallucinated),
-                                                 np.squeeze(utils.prepend_zero(Y)),
-                                                 batch_size=16)
-        hallucinated_preds = trainer.predict_y(ncr, hallucinated_dataloader)[1:, 0]
+            utils.prepend_zero(hallucinated),
+            np.squeeze(utils.prepend_zero(Y))
+        )[1:, 0]
         dists = np.abs([preds[i] - hallucinated_preds[i] for i in range(len(hallucinated_preds))])
         hallucinated_small = compressor.transform(hallucinated)
         min_pred = np.min(hallucinated_preds)
