84d7f8ce9886bb8ed976a31271d0de5040eda36b
Ben Lengerich <blengerich@gmail.com>
2022-11-02 10:59:16 -0400

    reformat print_acc_by_covar

--- scContextualized/analysis_utils.py
+++ scContextualized/analysis_utils.py
@@ -6,91 +6,55 @@ import numpy as np
 from sklearn.metrics import roc_auc_score as roc
 
 
-def print_acc_by_covars(
-    Y_train,
-    train_preds,
-    Y_test,
-    test_preds,
-    covar_df,
-    train_idx,
-    test_idx,
-    covar_stds=None,
-    covar_means=None,
-    covar_encoders=None,
-):
-    """
+def get_roc(Y_true, Y_pred):
+    """Measures ROC. Return np.nan is no ROC."""
+    try:
+        return roc(Y_true, Y_pred)
+    except (IndexError, ValueError):
+        return np.nan
 
-    :param Y_train:
-    :param train_preds:
-    :param Y_test:
-    :param test_preds:
-    :param covar_df:
-    :param train_idx:
-    :param test_idx:
-    :param covar_stds:  (Default value = None)
-    :param covar_means:  (Default value = None)
-    :param covar_encoders:  (Default value = None)
 
+def print_acc_by_covars(Y_true, Y_pred, covar_df, **kwargs):
     """
+    Prints Accuracy for different data splits with covariates.
+    Assume Y_true and Y_pred are np arrays.
+    Allows train_idx and test_idx as Boolean locators.
+    """
+    Y_true = np.squeeze(Y_true)
+    Y_pred = np.squeeze(Y_pred)
     for i, covar in enumerate(covar_df.columns):
         my_labels = covar_df.values[:, i]
-        if covar_stds is not None:
-            my_labels *= covar_stds[i]
-        if covar_means is not None:
-            my_labels += covar_means[i]
-        try:
-            my_labels = covar_encoders[i].inverse_transform(my_labels.astype(int))
-        except:
-            pass
-        if len(set(my_labels)) > 20:
+        if len(set(my_labels)) > kwargs.get("max_classes", 20):
             continue
+        if kwargs.get("covar_stds", None) is not None:
+            my_labels *= kwargs["covar_stds"][i]
+        if kwargs.get("covar_means", None) is not None:
+            my_labels += kwargs["covar_means"][i]
+        if kwargs.get("covar_encoders", None) is not None:
+            try:
+                my_labels = kwargs["covar_encoders"][i].inverse_transform(
+                    my_labels.astype(int)
+                )
+            except (AttributeError, TypeError, ValueError):
+                pass
         print("=" * 20)
         print(covar)
         print("-" * 10)
+
         for label in sorted(set(my_labels)):
-            try:
-                train_roc = roc(
-                    Y_train[my_labels[train_idx] == label],
-                    np.squeeze(train_preds)[my_labels[train_idx] == label],
+            label_idxs = my_labels == label
+            if (
+                kwargs.get("train_idx", None) is not None
+                and kwargs.get("test_idx", None) is not None
+            ):
+                my_train_idx = np.logical_and(label_idxs, kwargs["train_idx"])
+                my_test_idx = np.logical_and(label_idxs, kwargs["test_idx"])
+                train_roc = get_roc(Y_true[my_train_idx], Y_pred[my_train_idx])
+                test_roc = get_roc(Y_true[my_test_idx], Y_pred[my_test_idx])
+                print(
+                    f"{label}:\t Train ROC: {train_roc:.2f}, Test ROC: {test_roc:.2f}"
                 )
-            except (IndexError, ValueError):
-                train_roc = np.nan
-            try:
-                test_roc = roc(
-                    Y_test[my_labels[test_idx] == label],
-                    np.squeeze(test_preds)[my_labels[test_idx] == label],
-                )
-            except (IndexError, ValueError):
-                test_roc = np.nan
-            print(
-                "{}:\t Train ROC: {:.2f}, Test ROC: {:.2f}".format(
-                    label, train_roc, test_roc
-                )
-            )
+            else:
+                overall_roc = get_roc(Y_true[label_idxs], Y_pred[label_idxs])
+                print(f"{label}:\t ROC: {overall_roc:.2f}")
         print("=" * 20)
-
-
-"""
-def add_simulated_effect():
-    """ """
-    #
-
-
-def test_recovery_of_context_effects():
-    """ """
-    # TODO: test the recovery of simulated effects
-
-    C
-
-def test_recovery_of_homogeneous_effects():
-    """ """
-    # For n iterations,
-    # For each effect size
-    # Make plot of recoveries and return streng
-    # TODO: Recovery depends on strength of effect?
-
-
-def test_recovery_of_heterogeneous_effects():
-    """ """
-    # TODO: Recovery deends on strength of effect and distribution of C? C, X must be similar.
-"""
